2d1ed598a2b8a46a7c39e00208d1aa0dbd341176
adam-ubuntu <gy.adam90@gmail.com>
2021-01-01 14:44:47 -0800

    Update console logs

--- routes/exercises.js
+++ routes/exercises.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 let Exercise = require("../models/exercise.model");
 
 router.route("/").get((req, res) => {
-  console.log("GET /");
+  console.log("GET /exercises");
   Exercise.find()
     .then((exercises) => res.json(exercises))
     .catch((err) => res.status(400).json("Error: " + err));
