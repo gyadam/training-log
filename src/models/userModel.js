82ba2baba45a91f2701eb3c289cba9c857804726
adam-gyarmati <gyadam90@gmail.com>
2022-07-28 19:09:23 -0600

    update user model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,12 +2,25 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    username: {
+    email: {
       type: String,
       required: true,
       trim: true,
       minlength: 3,
     },
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    height: Number,
+    weight: Number,
+    dob: String, // dob format: YYYY-MM-DD https://stackoverflow.com/questions/6764821
   },
   {
     timestamps: true,
